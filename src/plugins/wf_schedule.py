99e087d221e3c405463b9effb6aa872777ee52c6
oneTAN <2690256773@qq.com>
2023-10-21 01:11:06 +0800

    Update wf_schedule.py
    消灭了驼峰

--- src/plugins/wf_schedule.py
+++ src/plugins/wf_schedule.py
@@ -8,21 +8,21 @@ import datetime
 # from datetime import datetime
 
 
-def readFileJson(fileName):
+def readFileJson(file_name):
     # 包一下读取json
-    with open(fileName, "r", encoding="UTF-8") as f:
+    with open(file_name, "r", encoding="UTF-8") as f:
         data = json.load(f)
     return data
 
 
-def spliceLayer(layer1, layer2, box):
+def spliceLayer(layer1: Image, layer2: Image, box: tuple[int, int, int, int]) -> Image:
     img = Image.new('RGBA', layer1.size, (255, 255, 255, 0))
     img.paste(layer1, (0, 0), layer1)
     img.paste(layer2, box, layer2)
     return img
 
 
-def spliceLayerT(layer1, layer2, box):
+def spliceLayerT(layer1: Image, layer2: Image, box: tuple[int, int, int, int]) -> Image:
     img = Image.new('RGBA', layer2.size, (255, 255, 255, 0))
     img.paste(layer1, box, layer1)
     img.paste(layer2, (0, 0), layer2)
@@ -71,49 +71,54 @@ def nineSliceUi(img: Image, box: tuple[int, int, int, int], size: tuple[int, int
 
 
 # 这张图的1天有多少px
-unitDayHeight = 144
+unit_day_height = 144
 
 # 总的pick up安排
-boss_battle_multi_pickup_event = readFileJson(r"data/wf_schedule/boss_battle_multi_pickup_event.json")
+boss_battle_multi_pickup_event = readFileJson(
+    r"data/wf_schedule/boss_battle_multi_pickup_event.json")
 # 每次pick up的详细安排
-boss_battle_multi_pickup_event_schedule = readFileJson(r"data/wf_schedule/boss_battle_multi_pickup_event_schedule.json")
+boss_battle_multi_pickup_event_schedule = readFileJson(
+    r"data/wf_schedule/boss_battle_multi_pickup_event_schedule.json")
 
 # 读取当前时间
-currentTime = datetime.datetime.now()
+current_time = datetime.datetime.now()
 
 for item in boss_battle_multi_pickup_event:
     # 我也不知道有啥用, 姑且作为底图的名字吧
-    codeName = boss_battle_multi_pickup_event[item][0]
+    code_name = boss_battle_multi_pickup_event[item][0]
 
     # 掉落倍率(加算)
     multiplier = boss_battle_multi_pickup_event[item][3]
 
     # 文件里储存的是str, 要转成datetime
-    startTime = datetime.datetime.strptime(boss_battle_multi_pickup_event[item][6], '%Y-%m-%d %H:%M:%S')
-    endTime = datetime.datetime.strptime(boss_battle_multi_pickup_event[item][7], '%Y-%m-%d %H:%M:%S')
+    start_time = datetime.datetime.strptime(
+        boss_battle_multi_pickup_event[item][6], '%Y-%m-%d %H:%M:%S')
+    end_time = datetime.datetime.strptime(
+        boss_battle_multi_pickup_event[item][7], '%Y-%m-%d %H:%M:%S')
 
     # 找到当前时间对应的pick up内容
-    if (currentTime >= startTime) and (currentTime <= endTime):
-        puNumber = item
+    if (current_time >= start_time) and (current_time <= end_time):
+        pu_number = item
         break
 
-text = [f"{startTime.strftime('%Y年%m月%d日%H:%M')}~{endTime.strftime('%Y年%m月%d日%H:%M')}",
+text = [f"{start_time.strftime('%Y年%m月%d日%H:%M')}~{end_time.strftime('%Y年%m月%d日%H:%M')}",
         "活动期间以协力战斗挑战特选对象领主",
         f"可获得的领主币、经验值和玛纳将变为{multiplier}倍!"]
-duration = (endTime-startTime).days + 2  # 活动持续时间
+duration = (end_time-start_time).days + 2  # 活动持续时间
 
-backgroundLayer = Image.new('RGBA', (1170, 470 + unitDayHeight * duration), (255, 255, 255, 0))
+background_layer = Image.new(
+    'RGBA', (1170, 470 + unit_day_height * duration), (255, 255, 255, 0))
 
-headImg = Image.open(r'data/wf_schedule/sprite_sheet/head.png')
-bodyImg = Image.open(r'data/wf_schedule/sprite_sheet/body.png')
-bodyImg = bodyImg.resize((1170, -42 + unitDayHeight * duration))
-footImg = Image.open(r'data/wf_schedule/sprite_sheet/foot.png')
+head_img = Image.open(r'data/wf_schedule/sprite_sheet/head.png')
+body_img = Image.open(r'data/wf_schedule/sprite_sheet/body.png')
+body_img = body_img.resize((1170, -42 + unit_day_height * duration))
+foot_img = Image.open(r'data/wf_schedule/sprite_sheet/foot.png')
 
-backgroundLayer.paste(headImg, (0, 0),)
-backgroundLayer.paste(bodyImg, (0, 256),)
-backgroundLayer.paste(footImg, (0, 214 + unitDayHeight * duration))
+background_layer.paste(head_img, (0, 0),)
+background_layer.paste(body_img, (0, 256),)
+background_layer.paste(foot_img, (0, 214 + unit_day_height * duration))
 
-draw = ImageDraw.Draw(backgroundLayer)
+draw = ImageDraw.Draw(background_layer)
 ft = ImageFont.truetype(r"data/wf_schedule/SY.ttf", 37)
 ft2 = ImageFont.truetype(r"data/wf_schedule/SY.ttf", 33)
 draw.text((96, 256), text[0], font=ft, fill="#525252")
@@ -123,56 +128,71 @@ draw.text((291, 320), text[1][5:9], font=ft, fill="#FF9933")
 draw.text((252, 368), text[2][4:-2], font=ft, fill="#FF9933")
 
 
-lineImg = Image.open(r"data/wf_schedule/sprite_sheet/line.png").convert('RGBA')
-drawDate = startTime
+line_img = Image.open(
+    r"data/wf_schedule/sprite_sheet/line.png").convert('RGBA')
+drawDate = start_time
 
 for i in range(0, duration + 1):
     if i < duration - 1:
-        backgroundLayer.paste(lineImg, (57, 570 + unitDayHeight*i), lineImg)
-    draw.text((56, 466 + unitDayHeight*i), drawDate.strftime('%m月%d日'), font=ft2, fill="#525252")
+        background_layer.paste(
+            line_img, (57, 570 + unit_day_height*i), line_img)
+    draw.text((56, 466 + unit_day_height*i),
+              drawDate.strftime('%m月%d日'), font=ft2, fill="#525252")
     drawDate += datetime.timedelta(days=1)
 
-whiteDecoration = Image.open(r'data/wf_schedule/sprite_sheet/bottom_right_decoration_white.png').convert('RGBA')
-purpleDecoration = Image.open(r'data/wf_schedule/sprite_sheet/bottom_right_decoration_purple.png').convert('RGBA')
-redDecoration = Image.open(r'data/wf_schedule/sprite_sheet/bottom_right_decoration_red.png').convert('RGBA')
+decoration_white_img = Image.open(
+    r'data/wf_schedule/sprite_sheet/bottom_right_decoration_white.png').convert('RGBA')
+decoration_purple_img = Image.open(
+    r'data/wf_schedule/sprite_sheet/bottom_right_decoration_purple.png').convert('RGBA')
+decoration_red_img = Image.open(
+    r'data/wf_schedule/sprite_sheet/bottom_right_decoration_red.png').convert('RGBA')
 
-foregroundLayer = Image.new('RGBA', backgroundLayer.size, (255, 255, 255, 0))
-decorationLayer = Image.new('RGBA', backgroundLayer.size, (255, 255, 255, 0))
-draw = ImageDraw.Draw(foregroundLayer)
+blank_layer = Image.new('RGBA', background_layer.size, (255, 255, 255, 0))
+foreground_layer = blank_layer.copy()
+decoration_layer = blank_layer.copy()
+draw = ImageDraw.Draw(foreground_layer)
 
-for item in boss_battle_multi_pickup_event_schedule[puNumber]:
+for item in boss_battle_multi_pickup_event_schedule[pu_number]:
     # boss的编号, 这个有用, 要拿去提审thumbnail
-    bossNumber = boss_battle_multi_pickup_event_schedule[puNumber][item][1]
+    boss_number = boss_battle_multi_pickup_event_schedule[pu_number][item][1]
 
     # 同上, 每个小方块对应的起止时间, 从str转换成datetime
-    startTimeSon = datetime.datetime.strptime(boss_battle_multi_pickup_event_schedule[puNumber][item][6], '%Y-%m-%d %H:%M:%S')
-    endTimeSon = datetime.datetime.strptime(boss_battle_multi_pickup_event_schedule[puNumber][item][7], '%Y-%m-%d %H:%M:%S')
+    start_time_son = datetime.datetime.strptime(
+        boss_battle_multi_pickup_event_schedule[pu_number][item][6], '%Y-%m-%d %H:%M:%S')
+    end_time_son = datetime.datetime.strptime(
+        boss_battle_multi_pickup_event_schedule[pu_number][item][7], '%Y-%m-%d %H:%M:%S')
 
-    if (currentTime >= startTimeSon) and (currentTime <= endTimeSon):
-        decorationImg = purpleDecoration
-        fillColor = '#6E196F'
-        outlineColor = '#841E85'
+    if (current_time >= start_time_son) and (current_time <= end_time_son):
+        decoration_img = decoration_purple_img
+        fill_color = '#6E196F'
+        outline_color = '#841E85'
     else:
-        decorationImg = whiteDecoration
-        fillColor = '#FAFAFA'
-        outlineColor = '#FFFFFF'
+        decoration_img = decoration_white_img
+        fill_color = '#FAFAFA'
+        outline_color = '#FFFFFF'
 
+    # 不知道怎么筛选, 干脆固定不取第一个好了
+    # 不对啊那普通的pick up怎么办
     if (item != "1"):
-        draw.rounded_rectangle((218, 498 + int(int((startTimeSon - startTime).total_seconds())/(24*60*60/unitDayHeight)), 1103, 490 + int(int((endTimeSon - startTime).total_seconds())/(24*60*60/unitDayHeight))), radius=24, fill=fillColor, outline=outlineColor, width=5)
-        decorationLayer.paste(decorationImg, (836, 333 + int(int((endTimeSon - startTime).total_seconds())/(24*60*60/unitDayHeight))), decorationImg)
+        draw.rounded_rectangle((218, 498 + int(int((start_time_son - start_time).total_seconds())/(24*60*60/unit_day_height)), 1103, 490 + int(
+            int((end_time_son - start_time).total_seconds())/(24*60*60/unit_day_height))), radius=24, fill=fill_color, outline=outline_color, width=5)
+        decoration_layer.paste(decoration_img, (836, 333 + int(int((end_time_son -
+                               start_time).total_seconds())/(24*60*60/unit_day_height))), decoration_img)
 
-shadowImage = Image.new('RGB', backgroundLayer.size, (200, 200, 200))
-foregroundLayer1 = foregroundLayer.copy()
-foregroundLayer2 = foregroundLayer.copy()
-foregroundLayer.paste(decorationLayer, (0, 0), foregroundLayer)
-foregroundLayer1.paste(shadowImage, (0, 0), foregroundLayer1)
-shadowImage = foregroundLayer1.filter(ImageFilter.GaussianBlur(6))
-backgroundLayer.paste(shadowImage, (0, 0), shadowImage)
-backgroundLayer.paste(foregroundLayer2, (0, 0), foregroundLayer2)
-backgroundLayer.paste(foregroundLayer, (0, 0), foregroundLayer)
-# backgroundLayer.paste(decorationLayer, (0, 0), foregroundLayer)
+blank_layer.paste(decoration_layer, (0, 0), foreground_layer)
+decoration_layer = blank_layer.copy()
+blank_layer = Image.new('RGBA', background_layer.size, (255, 255, 255, 0))
 
+shadow_layer = Image.new('RGB', background_layer.size, (200, 200, 200))
+blank_layer.paste(shadow_layer, (0, 0), foreground_layer)
+shadow_layer = blank_layer.copy()
+blank_layer = Image.new('RGBA', background_layer.size, (255, 255, 255, 0))
+shadow_layer = shadow_layer.filter(ImageFilter.GaussianBlur(6))
 
-backgroundLayer = backgroundLayer.convert('RGB')
-backgroundLayer.save(r"data/wf_schedule/test2.png")
-backgroundLayer.show()
+background_layer.paste(shadow_layer, (0, 0), shadow_layer)
+background_layer.paste(foreground_layer, (0, 0), foreground_layer)
+background_layer.paste(decoration_layer, (0, 0), decoration_layer)
+
+background_layer = background_layer.convert('RGB')
+background_layer.save(r"data/wf_schedule/test2.png")
+background_layer.show()
